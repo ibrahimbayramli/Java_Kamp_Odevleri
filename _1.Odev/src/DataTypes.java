88a7f87df210990488900572faa3c8e2fccfa022
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-09-29 19:32:57 +0300

    Odev 1

---
+++ _1.Odev/src/DataTypes.java
@@ -0,0 +1,8 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int sayi=12;
+        sayi=13;
+
+
+    }
+}
---
+++ _1.Odev/src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int ogrenciSayisi=112;
+        String mesaj="Ogrenci sayisi: ";
+
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+
+
+    }
+}
