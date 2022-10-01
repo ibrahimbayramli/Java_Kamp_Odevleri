408ed6a2ec24e2497d52757d58e659e0144a5364
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-10-01 16:44:30 +0300

    Odev 1

--- _1.Odev/src/SayiAsalMi.java
+++ _1.Odev/src/SayiAsalMi.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class SayiAsalMi {
     public static void main(String[] args) {
-
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Kontrol edilecek sayiyi giriniz: ");
+        Integer myNum=scan.nextInt();
+        scan.close();
+        Boolean isPrime=true;
+        for(int i=2;i<myNum;i++){
+            if(myNum%i==0 && myNum==1){
+                isPrime=false;
+                break;
+            }
+        }
+        if (isPrime){
+            System.out.println("Girdiginiz sayi asaldir.");
+        }else {
+            System.out.println("Girdiginiz sayi asaldegildir.");
+        }
     }
 }
